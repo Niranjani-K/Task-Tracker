da66f86324cdae2809a9a5715842df5cb8fe8bd6
Niranjani133 <niranjani.koteshwar@gmail.com>
2021-09-22 14:46:10 +0530

    Trying by removing local use

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ app.set('view engine' , 'ejs');
 app.use(express.static("public"));
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+
 app.listen(port);
 
 app.listen(port,function(){
